1ee1f8f44307c20af344f1a4931f315af1763e0b
Alexander201519 <xaxan.Ali2015@gmail.com>
2026-02-21 16:46:19 +0400

    Update app.py

--- app.py
+++ app.py
@@ -148,9 +148,12 @@ with tab4:
             st.success(f"BMI: {bmi:.2f}")
         else:
             st.error("Height must be greater than 0")
-mark.down(""")
+          
+st.markdown(
+    """
 
 components.html(ad_code, height=100)
+)
 
 
 
